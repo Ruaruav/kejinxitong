7c56292573974b765260d4c476375a22e7f77ca7
Ruaruav <78398650+Ruaruav@users.noreply.github.com>
2021-03-08 15:07:38 +0800

    Add files via upload
    好耶

---
+++ Defines.h
@@ -0,0 +1,28 @@
+#define LL long long
+#include <bits/stdc++.h>
+struct Date
+{
+	int year,month,day,hour,minute;
+	//  0 <= hour <= 23
+	// 0 <= minute <= 59
+};
+struct Yonghu
+{
+	long long id;//8位，不足补零
+	string password;//
+	int zhuangtai;
+	//0 正常 1 挂失 2 已注销
+	string nickname;
+	bool sex;// 0 nan 1 nv
+	Date birthday;
+	int money;
+	// void xiaofeijilu()
+	// {
+	// 	Find(id,1,);
+	// }
+	int Lv;
+	//1~5
+	int jingyan;
+	// 100经验升1级
+	Date daoqi;
+};
